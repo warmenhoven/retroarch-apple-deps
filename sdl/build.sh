37251c0e40426bd16159a907280309434b3cb157
Eric Warmenhoven <eric@warmenhoven.org>
2024-08-16 17:19:46 -0400

    Add build script for SDL2

---
+++ sdl/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function install() {
+    platform=$1
+    mkdir -p $platform/include
+    mv build/$platform/include-config-release/SDL2/*.h $platform/include
+    mkdir -p $platform/lib
+    mv build/$platform/libSDL2.a $platform/lib
+}
+
+echo Configuring for macOS
+cmake ${SDL_SRC:-SDL} -B build/macOS -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -DBUILD_SHARED_LIBS=OFF --log-level=ERROR
+echo Building for macOS
+cmake --build build/macOS -j
+echo Installing for macOS
+mkdir -p include
+mv build/macOS/include/SDL2/*.h include
+install macOS
+
+echo Configuring for iOS
+cmake ${SDL_SRC:-SDL} -B build/iOS -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF --log-level=ERROR
+echo Building for iOS
+cmake --build build/iOS -j
+echo Installing for iOS
+install iOS
