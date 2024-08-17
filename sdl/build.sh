d8f91f3896b46a91b58bf485a3683def51d0ab03
Eric Warmenhoven <eric@warmenhoven.org>
2024-08-17 10:09:14 -0400

    rebuilt sdl2 for macos using xcode 12.2

--- sdl/build.sh
+++ sdl/build.sh
@@ -9,17 +9,17 @@ function install() {
 }
 
 echo Configuring for macOS
-cmake ${SDL_SRC:-SDL} -B build/macOS -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DBUILD_SHARED_LIBS=OFF --log-level=ERROR
+cmake ${SDL_SRC:-SDL} -B build/macOS -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DBUILD_SHARED_LIBS=OFF
 echo Building for macOS
-cmake --build build/macOS -j
+cmake --build build/macOS
 echo Installing for macOS
 mkdir -p include
 mv build/macOS/include/SDL2/*.h include
 install macOS
 
 echo Configuring for iOS
-cmake ${SDL_SRC:-SDL} -B build/iOS -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF --log-level=ERROR
+cmake ${SDL_SRC:-SDL} -B build/iOS -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
 echo Building for iOS
-cmake --build build/iOS -j
+cmake --build build/iOS
 echo Installing for iOS
 install iOS
